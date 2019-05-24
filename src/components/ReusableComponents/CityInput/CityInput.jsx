3e2d703f375093addbab1324307affd2bb4e5403
Tamrikoo <tamara1kharkov@gmail.com>
2019-05-24 20:59:52 +0300

    added css search form1.6

---
+++ src/components/ReusableComponents/CityInput/CityInput.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import classes from './CityInput.module.css';
+
+
+
+const CityInput=({
+  field: { name, value, onChange, onBlur },
+  form: { errors, touched, setFieldValue },
+  ...props
+}) =>{
+
+    return(
+                      <div>
+                          <label htmlFor="city"> {props.values} near</label>
+                            <input
+                                className={classes.cityInput}
+                                type={props.type}
+                                placeholder="Enter city"
+                                id={props.id}
+                                name={name}
+                                onChange={onChange}
+                                value={value}
+                                        />
+                        </div>
+
+  )
+}
+
+
+
+export default CityInput;
