09517ff3f2ef1c9607128291dce79e9ff2dbe079
Tamrikoo <tamara1kharkov@gmail.com>
2019-05-29 13:39:52 +0300

    slider salary css

--- src/components/ReusableComponents/CityInput/CityInput.jsx
+++ src/components/ReusableComponents/CityInput/CityInput.jsx
@@ -4,43 +4,11 @@ import  { Select } from 'antd';
 import 'antd/lib/select/style/index.css';
 import data from '../../constants/data'
 
-// const CityInput=({
-//   field: { name, value, onChange, onBlur },
-//   form:{setFieldValue},
-//   ...props
-// }) =>{
-// const { Option } = Select;
-//     return(
-//                       <div >
-//                           <label htmlFor="city"> {props.values} near</label>
-//                           <Select
-//                               showSearch
-//                               id={props.id}
-//                               className={classes.cityInput}
-//                               size="large"
-//                               value={value}
-//                               placeholder="Enter City"
-//                               optionFilterProp="children"
-//                               onChange={(value)=>{
-//                                 setFieldValue('city', value)
-//                               }}
-//                               filterOption={(input, option) =>
-//                                 option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-//                               }
-//                             >
-//                           {data.cities.map(data=>  <Option value={data.content} key={Math.random()}>{data.content}</Option>)}
-
-//                           </Select>
-//                         </div>
-
-//   )
-// }
-
 
 
 const CityInput=({
   field: { name, value, onChange, onBlur },
-  form:{setFieldValue, handleChange, errors, touched},  
+  form:{setFieldValue, handleChange, errors, touched},
   ...props
 }) =>{
 const { Option } = Select;
@@ -72,6 +40,7 @@ const { Option } = Select;
                             >
                           {data.cities.map(data=>  <Option value={data.content} key={Math.random()}>{data.content}</Option>)}
                           </Select>
+                          
                         </div>
 
   )
